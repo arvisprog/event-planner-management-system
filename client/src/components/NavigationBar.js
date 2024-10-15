2e5cff43779b0af402c83c9f7b9af2dc26a2e77a
arvisprog <asulillari1@gmail.com>
2024-10-15 13:24:30 +0200

    fixed navigation bar css

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -1,20 +1,28 @@
 import "../styles/Navigation.css";
 import Logo from "../images/logo.png";
 
-function NavigationBar({ name }) {
+function NavigationBar({}) {
   return (
-    <nav class="navbar">
-      <div class="logo">
-        <img src={Logo} alt="Logo" />
+    <nav className="navbar">
+      <div className="logo">
+        <a href="/home">
+          <img src={Logo} alt="Logo" />{" "}
+        </a>
       </div>
 
-      <div class="menu-right">
-        <a href="#">Home</a>
-        <a href="#">Create Event</a>
-        <a href="#" class="cta-button">
-          Hello, {name}
+      <div className="menu-right">
+        <a className="link" href="#">
+          Home
         </a>
-        <a href="#">Logout</a>
+        <a className="link" href="#">
+          Create Event
+        </a>
+        <a className="cta-button" href="#">
+          Hello, {}
+        </a>
+        <a className="link" href="#">
+          Logout
+        </a>
       </div>
     </nav>
   );
