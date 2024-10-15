1842d25b711ea7e4dbe3c00d4206d4dc62e59b99
arvisprog <asulillari1@gmail.com>
2024-10-15 13:28:20 +0200

    fixed button css

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -1,7 +1,7 @@
 import "../styles/Navigation.css";
 import Logo from "../images/logo.png";
 
-function NavigationBar({}) {
+function NavigationBar({ name }) {
   return (
     <nav className="navbar">
       <div className="logo">
@@ -11,14 +11,14 @@ function NavigationBar({}) {
       </div>
 
       <div className="menu-right">
-        <a className="link" href="#">
+        <a className="link" href="/home">
           Home
         </a>
         <a className="link" href="#">
           Create Event
         </a>
         <a className="cta-button" href="#">
-          Hello, {}
+          Hello, {name}
         </a>
         <a className="link" href="#">
           Logout
