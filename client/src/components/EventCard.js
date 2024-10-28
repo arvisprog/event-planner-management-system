92552964674d77b4d587ce269b55f00afb5a7a1e
arvisprog <asulillari1@gmail.com>
2024-10-28 15:06:48 +0100

    separated event card code into its own component

---
+++ client/src/components/EventCard.js
@@ -0,0 +1,127 @@
+import React from "react";
+import { IoLocationOutline } from "react-icons/io5";
+import axios from "axios";
+import "../styles/Event.css";
+
+function EventCard({
+  eventId,
+  eventDate,
+  eventName,
+  eventLocation,
+  eventDescription,
+  attendeesCount,
+  isOwnEvent,
+  isUserAttending,
+  openModal,
+  getEvents,
+}) {
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  const dateStyleClass = isOwnEvent ? "date" : "event-date";
+
+  const joinEvent = async (eventId) => {
+    try {
+      await axios.post(
+        "http://localhost:8000/api/attendees",
+        { eventId },
+        { headers: { Authorization: `Bearer ${user?.token}` } }
+      );
+      getEvents();
+    } catch (error) {
+      console.error("Error joining event:", error);
+    }
+  };
+
+  const leaveEvent = async (eventId) => {
+    await axios
+      .delete(`http://localhost:8000/api/attendees/events/${eventId}`, {
+        headers: { Authorization: `Bearer ${user?.token}` },
+      })
+      .then(() => {
+        getEvents();
+      })
+      .catch((error) => {
+        console.error("Error leaving event:", error);
+      });
+  };
+
+  const deleteEvent = async (eventId) => {
+    if (window.confirm("Are you sure you want to delete this event?")) {
+      await axios
+        .delete(`http://localhost:8000/api/events/${eventId}`, {
+          headers: { Authorization: `Bearer ${user?.token}` },
+        })
+        .then(() => {
+          getEvents();
+        })
+        .catch((error) => {
+          console.error("Error leaving event:", error);
+        });
+    }
+  };
+
+  return (
+    <div className="basic-card basic-card-light" key={eventId}>
+      <div className="card-content">
+        <p className={"card-text " + dateStyleClass}>{eventDate}</p>
+        <span className="card-title">{eventName}</span>
+        <p className="card-text">
+          <IoLocationOutline style={{ marginRight: "5px" }} />
+          {eventLocation}
+        </p>
+        <p className="card-text description">{eventDescription}</p>
+        {attendeesCount > 0 && (
+          <p className="card-text attendees">{attendeesCount} attendees</p>
+        )}
+      </div>
+
+      <div className="card-link">
+        {isOwnEvent ? (
+          <>
+            <button
+              className="edit-button"
+              onClick={() =>
+                openModal(
+                  eventId,
+                  eventDate,
+                  eventName,
+                  eventLocation,
+                  eventDescription
+                )
+              }
+            >
+              Edit Event
+            </button>
+
+            <button
+              className="edit-button delete-button"
+              onClick={() => deleteEvent(eventId)}
+            >
+              Delete Event
+            </button>
+          </>
+        ) : (
+          <>
+            {isUserAttending ? (
+              <button
+                onClick={() => leaveEvent(eventId)}
+                className="leave-button edit-button"
+              >
+                Leave Event
+              </button>
+            ) : (
+              <button
+                onClick={() => joinEvent(eventId)}
+                className="edit-button"
+              >
+                Join Event
+              </button>
+            )}
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default EventCard;
--- client/src/components/home.js
+++ client/src/components/home.js
@@ -4,6 +4,7 @@ import { IoLocationOutline } from "react-icons/io5";
 import axios from "axios";
 import EventForm from "./EventForm";
 import "../styles/Event.css";
+import EventCard from "./EventCard";
 
 function Home() {
   const [events, setEvents] = useState([]);
@@ -35,47 +36,6 @@ function Home() {
       location: location,
       description: description,
     });
-  };
-
-  const joinEvent = async (eventId) => {
-    try {
-      await axios.post(
-        "http://localhost:8000/api/attendees",
-        { eventId },
-        { headers: { Authorization: `Bearer ${user?.token}` } }
-      );
-      getEvents();
-    } catch (error) {
-      console.error("Error joining event:", error);
-    }
-  };
-
-  const leaveEvent = async (eventId) => {
-    await axios
-      .delete(`http://localhost:8000/api/attendees/events/${eventId}`, {
-        headers: { Authorization: `Bearer ${user?.token}` },
-      })
-      .then(() => {
-        getEvents();
-      })
-      .catch((error) => {
-        console.error("Error leaving event:", error);
-      });
-  };
-
-  const deleteEvent = async (eventId) => {
-    if (window.confirm("Are you sure you want to delete this event?")) {
-      await axios
-        .delete(`http://localhost:8000/api/events/${eventId}`, {
-          headers: { Authorization: `Bearer ${user?.token}` },
-        })
-        .then(() => {
-          getEvents();
-        })
-        .catch((error) => {
-          console.error("Error leaving event:", error);
-        });
-    }
   };
 
   async function getEvents() {
@@ -122,75 +82,25 @@ function Home() {
             day: "numeric",
           });
           const isOwnEvent = userId === event.userId;
-          const dateStyleClass = isOwnEvent ? "date" : "event-date";
           const attendeesCount = event.Attendees.length;
           const isUserAttending = event.Attendees.some(
             (attendee) => attendee.userId === userId
           );
 
           return (
-            <div className="basic-card basic-card-light" key={event.id}>
-              <div className="card-content">
-                <p className={"card-text " + dateStyleClass}>{formattedDate}</p>
-                <span className="card-title">{event.name}</span>
-                <p className="card-text">
-                  <IoLocationOutline style={{ marginRight: "5px" }} />
-                  {event.location}
-                </p>
-                <p className="card-text description">{event.description}</p>
-                {attendeesCount > 0 && (
-                  <p className="card-text attendees">
-                    {attendeesCount} attendees
-                  </p>
-                )}
-              </div>
-
-              <div className="card-link">
-                {isOwnEvent ? (
-                  <>
-                    <button
-                      className="edit-button"
-                      onClick={() =>
-                        openModal(
-                          event.id,
-                          event.date,
-                          event.name,
-                          event.location,
-                          event.description
-                        )
-                      }
-                    >
-                      Edit Event
-                    </button>
-
-                    <button
-                      className="edit-button delete-button"
-                      onClick={() => deleteEvent(event.id)}
-                    >
-                      Delete Event
-                    </button>
-                  </>
-                ) : (
-                  <>
-                    {isUserAttending ? (
-                      <button
-                        onClick={() => leaveEvent(event.id)}
-                        className="leave-button edit-button"
-                      >
-                        Leave Event
-                      </button>
-                    ) : (
-                      <button
-                        onClick={() => joinEvent(event.id)}
-                        className="edit-button"
-                      >
-                        Join Event
-                      </button>
-                    )}
-                  </>
-                )}
-              </div>
-            </div>
+            <EventCard
+              key={event.id}
+              eventId={event.id}
+              eventDate={formattedDate}
+              eventName={event.name}
+              eventLocation={event.location}
+              eventDescription={event.description}
+              attendeesCount={attendeesCount}
+              isOwnEvent={isOwnEvent}
+              isUserAttending={isUserAttending}
+              openModal={openModal}
+              getEvents={getEvents}
+            />
           );
         })}
       </div>
